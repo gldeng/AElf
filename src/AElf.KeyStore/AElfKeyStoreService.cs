c212854a4820f8abc0d6c47cf6d5fb9dffc60778
gldeng <realgldeng@gmail.com>
2023-11-28 11:49:05 +0800

    add experiment keystore code

---
+++ src/AElf.KeyStore/AElfKeyStoreService.cs
@@ -0,0 +1,20 @@
+﻿using AElf.Cryptography;
+using AElf.Cryptography.Exceptions;
+using AElf.Types;
+using Nethereum.KeyStore;
+
+namespace AElf.KeyStore
+{
+    public class AElfKeyStoreService : KeyStoreService
+    {
+        public string EncryptKeyStoreAsJson(string password, string privateKey)
+        {
+            var keyPair = CryptoHelper.FromPrivateKey(ByteArrayHelper.HexStringToByteArray(privateKey));
+            if (keyPair?.PrivateKey == null || keyPair.PublicKey == null)
+                throw new InvalidKeyPairException("Invalid keypair (null reference).", null);
+
+            var address = Address.FromPublicKey(keyPair.PublicKey);
+            return EncryptAndGenerateDefaultKeyStoreAsJson(password, keyPair.PrivateKey, address.ToBase58());
+        }
+    }
+}
